c50afcc383096dd191ff1fb5a9cf4fb7a91566d6
Bertram Scharpf <software@bertram-scharpf.de>
2024-08-15 01:12:33 +0200

    Tools for X selections and TMux buffers

---
+++ lib/neovim/tools/copy.rb
@@ -0,0 +1,78 @@
+#
+#  neovim/tools/copy.rb  --  Set X11 selection and Tmux buffer
+#
+
+
+module Kernel
+
+  private
+
+  def xsel data = nil, sel: :primary
+    if    ($xsel  ||= command? "xsel" ) then
+      cmd = [ $xsel]
+      case sel
+      when :primary   then cmd.push "-p"
+      when :secondary then cmd.push "-s"
+      when :clipboard then cmd.push "-b"
+      end
+      ci, co = "-i", "-o"
+    elsif ($xclip ||= command? "xclip") then
+      cmd = [ $xclip, "-selection", sel.to_s]
+      ci, co = "-i", "-o"
+    end
+    cmd or raise ScriptError, "Sorry, Neither xsel nor xclip seem to be installed."
+    if data then
+      cmd.push ci
+      cmd_write cmd, data
+    else
+      cmd.push co
+      cmd_read  cmd
+    end
+  end
+  def xsel! data, sel: :clipboard
+    xsel data, sel: sel
+  end
+
+  def tmuxbuf data = nil, name: nil
+    $tmux ||= command? "tmux"
+    $tmux or raise ScriptError, "Sorry, TMux doesn't seem to be installed."
+    args = []
+    if name then
+      args.push "-b", name
+    end
+    args.push "-"
+    if data then
+      cmd_write [ $tmux, "load-buffer", *args], data
+    else
+      cmd_read  [ $tmux, "save-buffer", *args]
+    end
+  end
+
+
+  def command? cmd
+    if cmd[ File::SEPARATOR] then
+      cmd if File.executable? cmd
+    else
+      (ENV[ "PATH"].split File::PATH_SEPARATOR).each { |p|
+        c = File.join p, cmd
+        return c if File.executable? c
+      }
+      nil
+    end
+  end
+
+  def cmd_write cmd, data
+    case data
+    when Array  then data = data.join $/
+    when String then nil
+    else             data = data.to_s
+    end
+    IO.popen cmd, "w" do |t| t.write data end
+  end
+
+  def cmd_read cmd
+    IO.popen cmd, "r" do |t| t.read end
+  end
+
+end
+
