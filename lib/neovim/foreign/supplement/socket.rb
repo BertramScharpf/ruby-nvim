b12ec0bcba57347fe30037ebe534f9c6ed638a27
Bertram Scharpf <software@bertram-scharpf.de>
2025-07-17 10:12:32 +0200

    Fix bug in Supplement surrogate

--- lib/neovim/foreign/supplement/socket.rb
+++ lib/neovim/foreign/supplement/socket.rb
@@ -8,13 +8,28 @@ begin
   require "supplement/socket"
 rescue LoadError
   require "socket"
+  class BasicSocket
+    private def with_close socket
+      if block_given? then
+        begin
+          yield socket
+        ensure
+          socket.close
+        end
+      else
+        socket
+      end
+    end
+  end
   class TCPServer
     alias accept_orig accept
     private :accept_orig
-    def accept
-      a = accept_orig
-      if block_given? then yield a else a end
-    end
+    def accept &block ; with_close accept_orig, &block ; end
+  end
+  class UNIXServer
+    alias accept_orig accept
+    private :accept_orig
+    def accept &block ; with_close accept_orig, &block ; end
   end
 end
 
